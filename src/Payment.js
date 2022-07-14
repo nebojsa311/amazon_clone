359fe93b40707f76f31811d903c5981db5afdd9c
Nebojsa Draganic <nebojsa311@gmail.com>
2022-07-14 20:37:10 +0200

    added some css to checkout page

--- src/Payment.js
+++ src/Payment.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length}items</Link>)
+        </h1>
         <div className="payment__section">
-          <div className="payment__title"></div>
-          <h3>Delivery Address</h3>
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
           <div className="payment__address">
             <p>{user?.email}</p>
             <p>The Main street</p>
@@ -18,22 +23,28 @@ function Payment() {
           </div>
         </div>
 
-        <div className="payment__section"></div>
-        <div className="payment__title">
-          <h3>Review items and delivery</h3>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
         </div>
-        <div className="payment__items">
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment method</h3>
+          </div>
+          <div className="payment__details">{/* Stripe magic will go */}</div>
         </div>
-        <div className="payment__section"></div>
       </div>
     </div>
   );
